0c8078fae8017e0ad24eab70de2d29a5ea69f8d9
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-07-06 09:51:18 +1000

    fix: Logger using static mod id issue
    fix: Not setting mod id in new instance
    feat: Default mod id now AoqiaBaseLogger

--- media/lua/shared/AoqiaZomboidUtils/logger.lua
+++ media/lua/shared/AoqiaZomboidUtils/logger.lua
@@ -9,59 +9,87 @@ local constants = require("AoqiaZomboidUtils/constants")
 --- @class logger
 local logger = {}
 
-logger.MOD_ID = nil
+logger.MOD_ID = "AoqiaBaseLogger"
 
-function logger.debug(...)
-    if not logger.MOD_ID then return end
+function logger:debug(...)
+    if not self.MOD_ID then return end
     if constants.IS_DEBUG == false then return end
 
-    print(string.format("[%s] [DEBUG] %s", logger.MOD_ID, string.format(...)))
+    print(string.format("[%s] [DEBUG] %s", self.MOD_ID, string.format(...)))
 end
 
-function logger.debug_server(...)
-    if not logger.MOD_ID then return end
+function logger:debug_shared(...)
+    if not self.MOD_ID then return end
     if constants.IS_DEBUG == false then return end
 
-    print(string.format("[%s/server] [DEBUG] %s", logger.MOD_ID, string.format(...)))
+    print(string.format("[%s/shared] [DEBUG] %s", self.MOD_ID, string.format(...)))
 end
 
-function logger.info(...)
-    if not logger.MOD_ID then return end
+function logger:debug_server(...)
+    if not self.MOD_ID then return end
+    if constants.IS_DEBUG == false then return end
 
-    print(string.format("[%s] [INFO] %s", logger.MOD_ID, string.format(...)))
+    print(string.format("[%s/server] [DEBUG] %s", self.MOD_ID, string.format(...)))
 end
 
-function logger.info_server(...)
-    if not logger.MOD_ID then return end
+function logger:info(...)
+    if not self.MOD_ID then return end
 
-    print(string.format("[%s/server] [INFO] %s", logger.MOD_ID, string.format(...)))
+    print(string.format("[%s] [INFO] %s", self.MOD_ID, string.format(...)))
 end
 
-function logger.warn(...)
-    if not logger.MOD_ID then return end
+function logger:info_shared(...)
+    if not self.MOD_ID then return end
 
-    print(string.format("[%s] [WARN] ### %s ###", logger.MOD_ID, string.format(...)))
+    print(string.format("[%s/shared] [INFO] %s", self.MOD_ID, string.format(...)))
 end
 
-function logger.warn_server(...)
-    if not logger.MOD_ID then return end
+function logger:info_server(...)
+    if not self.MOD_ID then return end
 
-    print(string.format("[%s/server] [WARN] ### %s ###", logger.MOD_ID, string.format(...)))
+    print(string.format("[%s/server] [INFO] %s", self.MOD_ID, string.format(...)))
 end
 
-function logger.error(...)
-    if not logger.MOD_ID then return end
+function logger:warn(...)
+    if not self.MOD_ID then return end
 
-    local str = string.format("[%s] [ERROR] ###### %s ######", logger.MOD_ID, string.format(...))
+    print(string.format("[%s] [WARN] ### %s ###", self.MOD_ID, string.format(...)))
+end
+
+function logger:warn_shared(...)
+    if not self.MOD_ID then return end
+
+    print(string.format("[%s/shared] [WARN] ### %s ###", self.MOD_ID, string.format(...)))
+end
+
+function logger:warn_server(...)
+    if not self.MOD_ID then return end
+
+    print(string.format("[%s/server] [WARN] ### %s ###", self.MOD_ID, string.format(...)))
+end
+
+function logger:error(...)
+    if not self.MOD_ID then return end
+
+    local str = string.format("[%s] [ERROR] ###### %s ######", self.MOD_ID, string.format(...))
 
     print(str)
     error(str, 1)
 end
 
-function logger.error_server(...)
-    if not logger.MOD_ID then return end
+function logger:error_shared(...)
+    if not self.MOD_ID then return end
 
-    local str = string.format("[%s/server] [ERROR] ###### %s ######", logger.MOD_ID, string.format(...))
+    local str = string.format("[%s/shared] [ERROR] ###### %s ######", self.MOD_ID, string.format(...))
+
+    print(str)
+    error(str, 1)
+end
+
+function logger:error_server(...)
+    if not self.MOD_ID then return end
+
+    local str = string.format("[%s/server] [ERROR] ###### %s ######", self.MOD_ID, string.format(...))
 
     print(str)
     error(str, 1)
@@ -70,11 +98,12 @@ end
 --- @param mod_id string
 --- @return logger instance
 function logger:new(mod_id)
+    ---@class logger
     local o = {}
     setmetatable(o, self)
 
     self.__index = self
-    logger.MOD_ID = mod_id
+    o.MOD_ID = mod_id
 
     return o
 end
