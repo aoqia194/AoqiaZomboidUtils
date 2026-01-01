1d750b0085a13a375ad38d671ab03b2c85bf22c0
aoqia <aoqia@aoqia.dev>
2026-01-01 16:18:46 +1100

    fix: Make script cross-compatible and fix path issues
    Signed-off-by: aoqia <aoqia@aoqia.dev>

--- scripts/generate_dist.py
+++ scripts/generate_dist.py
@@ -16,23 +16,46 @@ import re
 import shutil
 import sys
 
+EXCLUDED_PATTERNS = [
+    r"^.+\.(?:blend1|psd)$",
+]
+
+EXCLUDED_DIRS = [".vscode"]
+
+SCRIPT_PATH = pathlib.Path(__file__)
+ROOT_PATH = SCRIPT_PATH.parent.parent
+
+
+def copy_files(src: pathlib.Path, dest: pathlib.Path):
+    for dirpath, _, filenames in src.walk():
+        for excluded_dir in EXCLUDED_DIRS:
+            if dirpath.match(excluded_dir):
+                print("Matched excluded directory: ", excluded_dir)
+                break
+        else:  # No excluded directory found
+            for filename in filenames:
+                for pattern in EXCLUDED_PATTERNS:
+                    if re.match(pattern, filename):
+                        print("Matched excluded pattern: ", pattern)
+                        break
+                else:
+                    src_file = dirpath.joinpath(filename)
+                    dest_file = dest.joinpath(pathlib.Path(*src_file.relative_to(ROOT_PATH).parts[1:]))
+
+                    print(f"Copying file {src_file} to {dest_file}")
+
+                    if not dest_file.parent.exists():
+                        dest_file.parent.mkdir(parents=True)
+
+                    shutil.copy2(src_file, dest_file)
+
 
 def main():
     """Main entry point."""
 
-    script_path = pathlib.Path(__file__)
-    repo_path = script_path.parent.parent
-
-    src_path = repo_path.joinpath("src")
-    dist_path = repo_path.joinpath("dist")
-    workshop_path = repo_path.joinpath("workshop")
-
-    excluded_patterns = [
-        r"^.+\.blend1$",
-        r"^.+\.psd$",
-    ]
-
-    excluded_dirs = [".vscode"]
+    src_path = ROOT_PATH.joinpath("src")
+    dist_path = ROOT_PATH.joinpath("dist")
+    workshop_path = ROOT_PATH.joinpath("workshop")
 
     # Get mod id for later use
 
@@ -56,9 +79,7 @@ def main():
     if dist_path.exists():
         for it in dist_path.iterdir():
             os.remove(it.absolute())
-
         dist_path.rmdir()
-
     dist_path.mkdir()
 
     # Create workshop folder tree
@@ -68,52 +89,8 @@ def main():
     mod_path = mods.joinpath(mod_id)
     mod_path.mkdir(parents=True)
 
-    # Copy all source files to mod path in dest
-
-    for dirpath, _, filenames in src_path.walk():
-        for excluded_dir in excluded_dirs:
-            if dirpath.match(excluded_dir):
-                print("Matched excluded directory: ", excluded_dir)
-                break
-        else:  # No excluded directory found
-            for filename in filenames:
-                for pattern in excluded_patterns:
-                    if re.match(pattern, filename):
-                        print("Matched excluded pattern: ", pattern)
-                        break
-                else:
-                    src_file = dirpath.joinpath(filename)
-                    dest_file = mod_path.joinpath(pathlib.Path(*src_file.parts[4:]))
-
-                    print(f"Copying file {src_file} to {dest_file}")
-                    if not dest_file.parent.exists():
-                        dest_file.parent.mkdir(parents=True)
-
-                    shutil.copy2(src_file, dest_file)
-
-    # Copy all workshop files to workshop folder
-
-    for dirpath, _, filenames in workshop_path.walk():
-        for excluded_dir in excluded_dirs:
-            if dirpath.match(excluded_dir):
-                print("Matched excluded directory: ", excluded_dir)
-                break
-        else:  # No excluded directory found
-            for filename in filenames:
-                for pattern in excluded_patterns:
-                    if re.match(pattern, filename):
-                        print("Matched excluded pattern: ", pattern)
-                        break
-                else:
-                    src_file = dirpath.joinpath(filename)
-                    dest_file = dist_path.joinpath(pathlib.Path(*src_file.parts[4:]))
-
-                    print(f"Copying file {src_file} to {dest_file}")
-
-                    if not dest_file.parent.exists():
-                        dest_file.parent.mkdir(parents=True)
-
-                    shutil.copy2(src_file, dest_file)
+    copy_files(src_path, mod_path)
+    copy_files(workshop_path, dist_path)
 
     print("Dist files generated successfully.")
 
